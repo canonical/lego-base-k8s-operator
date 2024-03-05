b425d1962e5be04a1c223c0cf46a805408c0691f
Guillaume Belanger <guillaume.belanger@canonical.com>
2024-03-05 07:14:01 -0500

    feat: add log forwarding support (#62)

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -15,12 +15,15 @@ logger = logging.getLogger(__name__)
 METADATA = yaml.safe_load(Path("tests/integration/lego-operator/metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
 TLS_LIB_PATH = "lib/charms/tls_certificates_interface/v3/tls_certificates.py"
+LOKI_LIB_PATH = "lib/charms/loki_k8s/v1/loki_push_api.py"
 LEGO_CLIENT_LIB_PATH = "lib/charms/lego_base_k8s/v0/lego_client.py"
 LEGO_OPERATOR_DIR = "tests/integration/lego-operator"
+GRAFANA_AGENT_CHARM_NAME = "grafana-agent-k8s"
 
 
 def copy_lib_content() -> None:
     shutil.copyfile(src=TLS_LIB_PATH, dst=f"{LEGO_OPERATOR_DIR}/{TLS_LIB_PATH}")
+    shutil.copyfile(src=LOKI_LIB_PATH, dst=f"{LEGO_OPERATOR_DIR}/{LOKI_LIB_PATH}")
     shutil.copyfile(src=LEGO_CLIENT_LIB_PATH, dst=f"{LEGO_OPERATOR_DIR}/{LEGO_CLIENT_LIB_PATH}")
 
 
@@ -50,3 +53,21 @@ async def test_build_and_deploy(ops_test):
         timeout=1000,
     )
     assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
+
+
+@pytest.mark.abort_on_fail
+async def test_given_grafana_agent_when_integrate_then_status_is_active(ops_test):
+    """Integrate with a logging endpoint."""
+    await ops_test.model.deploy(
+        GRAFANA_AGENT_CHARM_NAME,
+        application_name=GRAFANA_AGENT_CHARM_NAME,
+        channel="stable",
+    )
+    await ops_test.model.integrate(
+        relation1=f"{APP_NAME}:logging", relation2=GRAFANA_AGENT_CHARM_NAME
+    )
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME],
+        status="active",
+        timeout=1000,
+    )
